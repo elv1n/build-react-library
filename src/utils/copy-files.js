713bc1614a48c2e139f254dff0fb0ffed4e1e116
Elvin <elvin.d@outlook.com>
2019-03-15 10:13:48 +0200

    index.d.ts is not required

--- src/utils/copy-files.js
+++ src/utils/copy-files.js
@@ -37,14 +37,10 @@ async function createModulePackages({ from, to }) {
       };
       const packageJsonPath = path.join(to, directoryPackage, 'package.json');
 
-      const [typingsExist] = await Promise.all([
+      await Promise.all([
         fse.exists(path.join(to, directoryPackage, 'index.d.ts')),
         fse.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2))
       ]);
-
-      if (!typingsExist) {
-        throw new Error(`index.d.ts for ${directoryPackage} is missing`);
-      }
 
       return packageJsonPath;
     })
